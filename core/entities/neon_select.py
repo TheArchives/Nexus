73ae408c20e1c2d4f2e765623d0897b621729a74
Gareth Coles <colesgareth2@hotmail.com>
2012-09-30 05:25:40 +0100

    Licensing

--- core/entities/neon_select.py
+++ core/entities/neon_select.py
@@ -1,38 +1,10 @@
-#    iCraft is Copyright 2010-2011 both
+# The Nexus software is licensed under the BSD 2-Clause license.
 #
-#    The Archives team:
-#                   <Adam Guy> adam@adam-guy.com AKA "Adam01"
-#                   <Andrew Godwin> andrew@aeracode.org AKA "Aera"
-#                   <Dylan Lukes> lukes.dylan@gmail.com AKA "revenant"
-#                   <Gareth Coles> colesgareth2@hotmail.com AKA "gdude2002"
+# You should have recieved a copy of this license with the software.
+# If you did not, you can find one at the following link.
 #
-#    And,
-#
-#    The iCraft team:
-#                   <Andrew Caluzzi> tehcid@gmail.com AKA "tehcid"
-#                   <Andrew Dolgov> fox@bah.org.ru AKA "gothfox"
-#                   <Andrew Horn> Andrew@GJOCommunity.com AKA "AndrewPH"
-#                   <Brad Reardon> brad@bradness.co.cc AKA "PixelEater"
-#                   <Clay Sweetser> CDBKJmom@aol.com AKA "Varriount"
-#                   <James Kirslis> james@helplarge.com AKA "iKJames"
-#                   <Jason Sayre> admin@erronjason.com AKA "erronjason"
-#                   <Jonathon Dunford> sk8rjwd@yahoo.com AKA "sk8rjwd"
-#                   <Joseph Connor> destroyerx100@gmail.com AKA "destroyerx1"
-#                   <Kamyla Silva> supdawgyo@hotmail.com AKA "NotMeh"
-#                   <Kristjan Gunnarsson> kristjang@ffsn.is AKA "eugo"
-#                   <Nathan Coulombe> NathanCoulombe@hotmail.com AKA "Saanix"
-#                   <Nick Tolrud> ntolrud@yahoo.com AKA "ntfwc"
-#                   <Noel Benzinger> ronnygmod@gmail.com AKA "Dwarfy"
-#                   <Randy Lyne> qcksilverdragon@gmail.com AKA "goober"
-#                   <Willem van der Ploeg> willempieeploeg@live.nl AKA "willempiee"
-#
-#    Disclaimer: Parts of this code may have been contributed by the end-users.
-#
-#    iCraft is licensed under the Creative Commons
-#    Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
-#    To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
-#    Or, send a letter to Creative Commons, 171 2nd Street,
-#    Suite 300, San Francisco, California, 94105, USA.
+# http://opensource.org/licenses/bsd-license.php
+
 
 if len(parts) < 3:
     self.client.sendServerMessage("Please give a color (blue, red, white, green)")
--- core/plugins/zones.py
+++ core/plugins/zones.py
@@ -1,4 +1,10 @@
-#    iCraft is Copyright 2010-2011 both
+# The Nexus software is licensed under the BSD 2-Clause license.
+#
+# You should have recieved a copy of this license with the software.
+# If you did not, you can find one at the following link.
+#
+# http://opensource.org/licenses/bsd-license.php
+# http://opensource.org/licenses/bsd-license.php
 #
 #    The Archives team:
 #                   <Adam Guy> adam@adam-guy.com AKA "Adam01"
